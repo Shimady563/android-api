558df95ad644dfc7dbf09b1c276dd7a0049f74f7
Shimady563 <shimady563@gmail.com>
2025-05-02 22:25:11 +0300

    feat(gallery): implement painting api

---
+++ src/main/kotlin/com/shimady563/android/gallery/controller/PaintingController.kt
@@ -0,0 +1,32 @@
+package com.shimady563.android.gallery.controller
+
+import com.shimady563.android.gallery.model.dto.PaintingDto
+import com.shimady563.android.gallery.service.PaintingService
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/exhibitions")
+class PaintingController(private val paintingService: PaintingService) {
+
+    @GetMapping("")
+    fun getAllPaintings(): List<PaintingDto> {
+        return paintingService.getAllPaintings()
+    }
+
+    @PostMapping("")
+    fun createPainting(@Valid @RequestBody request: PaintingDto) {
+        paintingService.createPainting(request)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePainting(@PathVariable id: UUID, @Valid @RequestBody request: PaintingDto) {
+        return paintingService.updatePainting(id, request)
+    }
+
+    @DeleteMapping("{id}")
+    fun deletePainting(@PathVariable id: UUID) {
+        return paintingService.deletePaintingById(id)
+    }
+}
